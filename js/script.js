855ee3000249e95f024ad55b8cf6c07622e5b2b2
siddhant sahu <siddhantsahu503@gmail.com>
2023-01-21 18:55:27 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*===============================typing animation==========================*/
+var typed = new Typed(".typing",{
+    strings:["","Web Developer", "Web Desingner", "UI/UX Designer", "Full Stack Developer"],
+    typeSpeed:100,
+    BackSpeed: 60,
+    loop:true
+})
+
+let text = document.getElementById("myAnchor").download;
